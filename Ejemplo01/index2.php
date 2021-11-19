89716eb02fa49c9f03eb51c045ff842576358a64
Jorge Cisneros <fraxito@gmail.com>
2021-11-19 08:28:42 +0100

    Viernes!

---
+++ Ejemplo01/index2.php
@@ -0,0 +1,7 @@
+<?php
+$usuario_nombre = $_POST['usuario_nombre'];
+$usuario_clave = $_POST['usuario_clave'];
+
+echo "El nombre del usuario es: ".
+            $usuario_nombre."<br>";
+echo "La clave es: ".$usuario_clave;
