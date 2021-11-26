99660d2f09292e2cd99c5f7d504b71287798d0e6
Jorge Cisneros <fraxito@gmail.com>
2021-11-26 09:58:31 +0100

    CON LA QUERY A MYSQL INCORRECTA

--- Ejemplo01/index2.php
+++ Ejemplo01/index2.php
@@ -4,18 +4,27 @@ include("funciones.php");
 $usuario_nombre = $_POST['usuario_nombre'];
 $usuario_clave = $_POST['usuario_clave'];
 
-echo "El nombre del usuario es: ".
-            $usuario_nombre."<br>";
-echo "La clave es: ".$usuario_clave."<br>";
-
-
 
 $mysql = conectaBBDD();   //me conecto a la base de datos
 
-$miPrimeraConsulta = $mysql -> query("SELECT * FROM usuarios");
+$miPrimeraConsulta = $mysql -> 
+query("SELECT * FROM usuarios 
+                WHERE nombreUsuario = '$usuario_nombre' 
+                AND  claveUsuario = '$usuario_clave' 
+                ");
 $numUsuarios = $miPrimeraConsulta -> num_rows; 
 
-echo $numUsuarios;
+if ($numUsuarios > 0){
+    echo'<div class="alert alert-success" role="alert">
+            USUARIO Y CONTRASEÑA CORRECTOS
+        </div>';     
+}
+else{
+    echo'<div class="alert alert-danger" role="alert">
+            error no existe ese usuario o la contraseña es incorrecta
+        </div>';  
+}
+
 
 
 
